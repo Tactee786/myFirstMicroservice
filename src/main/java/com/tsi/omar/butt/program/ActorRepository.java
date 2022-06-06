65bb6e5f4bb7f1039af823a11b3ccec7156ac213
Tactee786 <o_ma_c_1997@hotmail.co.uk>
2022-06-06 12:04:54 +0100

    first commit

---
+++ src/main/java/com/tsi/omar/butt/program/ActorRepository.java
@@ -0,0 +1,7 @@
+package com.tsi.omar.butt.program;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ActorRepository extends CrudRepository<Actor,Integer> {
+
+}
---
+++ src/main/java/com/tsi/omar/butt/program/MyFirstMicroserviceApplication.java
@@ -0,0 +1,33 @@
+package com.tsi.omar.butt.program;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/")
+public class MyFirstMicroserviceApplication {
+	@Autowired
+	private ActorRepository actorRepository;
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MyFirstMicroserviceApplication.class, args);
+	}
+
+	public MyFirstMicroserviceApplication(ActorRepository actorRepository){
+		this.actorRepository =actorRepository;
+	}
+
+	@GetMapping("/All_Actors")
+	public @ResponseBody
+	Iterable<Actor>getAllActors(){
+		return  actorRepository.findAll();
+	}
+
+}
