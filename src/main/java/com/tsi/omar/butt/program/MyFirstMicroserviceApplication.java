1e99ea5786f7a052ea51251afe19ebd616e18b49
Tactee786 <o_ma_c_1997@hotmail.co.uk>
2022-06-07 09:10:36 +0100

    added getmapping and deletemapping for an actor per id

--- src/main/java/com/tsi/omar/butt/program/MyFirstMicroserviceApplication.java
+++ src/main/java/com/tsi/omar/butt/program/MyFirstMicroserviceApplication.java
@@ -3,10 +3,9 @@ package com.tsi.omar.butt.program;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @SpringBootApplication
 @RestController
@@ -23,10 +22,22 @@ public class MyFirstMicroserviceApplication {
 		this.actorRepository =actorRepository;
 	}
 
-	@GetMapping("/All_Actors")
+	@GetMapping("/Get_All_Actors")
 	public @ResponseBody
 	Iterable<Actor>getAllActors(){
 		return  actorRepository.findAll();
+	}
+
+	@GetMapping("/Get_A_Actor")
+	public @ResponseBody
+	Optional<Actor>getAActors(@RequestParam(name="id") int id){
+		return  actorRepository.findById(id);
+	}
+
+	@DeleteMapping("/Delete_By_Id")
+	public @ResponseBody
+	void deleteById(@RequestParam(name="id") int id){
+		actorRepository.deleteById(id);
 	}
 
 }
