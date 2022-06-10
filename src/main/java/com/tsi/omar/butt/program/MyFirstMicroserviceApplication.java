72478a520a42c9d6867e0d93e093c057ff0234b5
Tactee786 <o_ma_c_1997@hotmail.co.uk>
2022-06-10 15:03:24 +0100

    fixed unused string

--- src/main/java/com/tsi/omar/butt/program/MyFirstMicroserviceApplication.java
+++ src/main/java/com/tsi/omar/butt/program/MyFirstMicroserviceApplication.java
@@ -13,8 +13,6 @@ import org.springframework.web.bind.annotation.*;
 public class MyFirstMicroserviceApplication {
 	@Autowired
 	private ActorRepository actorRepository;
-	private String rubbishString;
-
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyFirstMicroserviceApplication.class, args);
