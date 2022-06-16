2c626c2109519202b11a148d77b80b12037f8d19
Tactee786 <o_ma_c_1997@hotmail.co.uk>
2022-06-16 18:46:33 +0100

    cleaned up selenium tests and removed redundent tests

--- src/test/java/com/tsi/omar/butt/program/seleniumTest.java
+++ src/test/java/com/tsi/omar/butt/program/seleniumTest.java
@@ -1,7 +1,7 @@
 package com.tsi.omar.butt.program;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -9,44 +9,37 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
-
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class seleniumTest {
-    public WebDriver driver;
-    @Test
-    public void chromeDriverTest(){
+    public static WebDriver driver;
+    @BeforeAll
+    public static void setup() {
         WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+    }
+    @AfterAll
+    public static void tearDown() {
         driver.quit();
     }
-
     @Test
-    public void chromeSessionTest(){
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions options = new ChromeOptions();
-        driver = new ChromeDriver(options);
-        driver.quit();
-    }
-
-    @Test
+    @Order(1)
     public void checkWebsiteConnection(){
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.get("http://localhost:3000/");
-        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
-        driver.quit();
+        Assertions.assertDoesNotThrow(() -> {
+            driver.get("http://localhost:3000/");
+        });
     }
 
     @Test
+    @Order(2)
     public void checkActorForm(){
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
         driver.get("http://localhost:3000/Actor");
-        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
         WebElement addId = driver.findElement(By.className("SFAAIField"));
         addId.sendKeys("3");
-        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
         WebElement submitButton = driver.findElement(By.className("SFAAISubmit"));
         submitButton.click();
-        driver.quit();
+        WebElement element = driver.findElement(By.xpath("//*[@id=\"root\"]/div[2]/ol"));
+        Assertions.assertTrue(element.getText().contains("ID : 3"), "not ID : 3");
+        Assertions.assertTrue(element.isDisplayed());
     }
 }
