ba2340cf62ed873a22798a5252d28418304f7a80
Tactee786 <o_ma_c_1997@hotmail.co.uk>
2022-06-22 15:08:54 +0100

    added junit test for the Category class

---
+++ src/test/java/com/tsi/omar/butt/program/CategoryTest.java
@@ -0,0 +1,34 @@
+package com.tsi.omar.butt.program;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CategoryTest {
+    @Test
+    void testGetLanguageId(){
+        Language testLanguage = new Language("testLanguage");
+        testLanguage.setLanguageId(1);
+        Assertions.assertEquals(1, testLanguage.getLanguageId(), "id not there.");
+    }
+    @Test
+    void testSetLanguageId(){
+        Language testLanguage = new Language("testLanguage");
+        testLanguage.setLanguageId(1);
+        Assertions.assertEquals(1, testLanguage.getLanguageId(), "id not set.");
+    }
+    @Test
+    void testGetLanguageName(){
+        Language testLanguage = new Language("testLanguage");
+        testLanguage.setLanguageId(1);
+        Assertions.assertEquals("testLanguage", testLanguage.getName(), "name not there.");
+    }
+    @Test
+    void testSetLanguageName(){
+        Language testLanguage = new Language();
+        testLanguage.setName("testLanguageName");
+        testLanguage.setLanguageId(1);
+        Assertions.assertEquals("testLanguageName", testLanguage.getName(), "name not set.");
+    }
+
+}
+
